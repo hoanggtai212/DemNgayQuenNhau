ac9cd13f095f4669ab267e9d003c18c2dcbb17b5
HT <rongchube99@gmail.com>
2026-06-06 20:13:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const startDate = '2022-05-23 00:00:00';
+    const startDate = '2026-05-23 00:00:00';
 
     function updateLoveTimer() {
         const startDateObj = new Date(startDate);
